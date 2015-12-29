ac124246717b9a308a3b75f33d8910f41656178c
liekkas <zjq0717@163.com>
2015-12-29 18:17:05 +0800

    组件和编辑器的Lookup

---
+++ src/constants/LookUp.js
@@ -0,0 +1,9 @@
+/**
+ * Created by liekkas on 15/12/29.
+ */
+import * as comp from '../components';
+
+export const Lookup = {
+  'LineChart': comp.LineChart,
+  'LineChartEditor': comp.LineChartEditor,
+};
