4beb1873460e0f8590d0057e7a5071ff8b5438dd
liekkas <zjq0717@163.com>
2016-01-07 21:13:07 +0800

    场景呈现导航界面

---
+++ src/containers/ShowNav/Cover.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Coverflow from 'react-coverflow';
+import style from './style.scss';
+import _ from 'lodash';
+import { screenCoverImages } from '../../constants/Consts';
+
+class Cover extends React.Component {
+
+  getCover(cover) {
+    const index = _.findIndex(screenCoverImages, 'name', cover);
+    return screenCoverImages[index > -1 ? index : 0].img;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.showScenes.length > 0;
+  }
+
+  render() {
+    console.log('>>> Cover:Render', this.props.showScenes);
+    const { showScenes } = this.props;
+    return (
+      <Coverflow displayQuantityOfSide={1}
+                 navigation={false}
+                 enableScroll={true}
+                 media={{
+                  '@media (max-width: 100vw)': {
+                    width: '100vw',
+                    height: '100vh'
+                  },
+                  '@media (min-width: 900px)': {
+                    width: '100vw',
+                    height: '100vh'
+                  }
+                }}>
+        {
+          showScenes.map(({ id, name, cover }, index) =>
+            <img className={style.img} key={index} src={this.getCover(cover)}
+                 alt={name} url={'http://localhost:3000/#/show/2'} />
+          )
+        }
+      </Coverflow>
+    );
+  }
+}
+
+Cover.propTypes = {
+  showScenes: React.PropTypes.array.isRequired,
+};
+Cover.defaultProps = {
+  showScenes: []
+};
+
+export default Cover;
---
+++ src/containers/ShowNav/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Coverflow from 'react-coverflow';
+import _ from 'lodash';
+import { screenCoverImages } from '../../constants/Consts';
+import style from './style.scss';
+import Cover from './Cover';
+
+class ShowNav extends React.Component {
+
+  handleClick() {
+    console.log('>>> ShowNav:clicked');
+  }
+
+  render() {
+    const { scenes } = this.props;
+    return (
+      <div className={style.root}>
+        {
+          scenes.entries.length > 0 ?
+            <Cover showScenes={scenes.entries} /> : null
+        }
+      </div>
+    );
+  }
+}
+
+ShowNav.propTypes = {
+  scenes: React.PropTypes.object.isRequired,
+};
+ShowNav.defaultProps = {
+  scenes: {
+    groups: [],
+    entries: [],
+  },
+};
+
+function select(state) {
+  //console.log('>>> Maker:Selector', state.get('global'), state.get('curScene'));
+  return {
+    themeP: state.getIn(['global', 'theme']),
+    screenRatio: state.getIn(['global', 'screenRatio']),
+    screenNums: state.getIn(['global', 'screenNums']),
+    user: state.getIn(['global', 'user']),
+    scenes: convert(state.get('scenes').toJS()),
+    //scenes: {
+    //  entries: [
+    //    { id:'111', name:'haha', cover:'春节' },
+    //    { id:'111', name:'haha', cover:'天际线' },
+    //    { id:'111', name:'haha', cover:'天际线' },
+    //    { id:'111', name:'haha', cover:'天际线' },
+    //  ]
+    //},
+  };
+}
+
+function convert(obj) {
+  const result = [];
+  _.forEach(obj.entries, function (item) {
+    result.push({ id: item.id, name: item.name, cover: item.cover });
+  });
+  return {
+    entries: result,
+  };
+}
+
+export default connect(select)(ShowNav);
