47aa84383be6e068e36931241c3d8f638fce6c7c
liekkas <zjq0717@163.com>
2016-01-07 21:13:31 +0800

    保存窗口完成

---
+++ src/containers/Maker/SaveWindow.jsx
@@ -0,0 +1,159 @@
+/**
+ * 场景保存窗口
+ */
+
+import React, { PropTypes } from 'react';
+import { MenuItem, DropDownMenu, TextField, AutoComplete, FlatButton, SelectField, Checkbox, RadioButton, RadioButtonGroup, Toggle } from 'material-ui';
+import Slider from 'react-slick';
+import style from './savewin.scss';
+import { themes, screenRatios, screenNums, screenCoverImages } from '../../constants/Consts';
+import _ from 'lodash';
+import dateFormat from 'dateFormat';
+
+class SaveWindow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handlerSubmit = this.handlerSubmit.bind(this);
+    this.state = {
+      name: this.props.name,
+      desc: this.props.desc,
+      group: this.props.group,
+      theme: this.props.theme,
+      cover: this.props.cover,
+      errorText: this.props.name ? '' : '* 必填',
+    };
+  }
+
+  getConfig() {
+    const { group, theme, cover } = this.state;
+    const name = this.refs.name.getValue();
+    const desc = this.refs.desc.getValue();
+    return { name, desc,
+      group: group.length === 0 ? '未分组' : group,
+      cover: cover.length === 0 ? screenCoverImages[0].name : cover,
+      state: 'saved', updateDate: dateFormat(new Date(), 'yyyy-mm-dd hh:MM:ss') };
+  }
+
+  getCoverIndex() {
+    const index = _.findIndex(screenCoverImages, 'name', this.state.cover);
+    return index > -1 ? index : 0;
+  }
+
+  handlerSubmit() {
+    if (this.valide()) {
+      this.props.onSave(this.getConfig());
+    }
+  }
+
+  valide() {
+    const name = this.refs.name.getValue();
+    if (name.length === 0) {
+      this.refs.name.focus();
+      return false;
+    }
+
+    //if (this.state.group.length === 0) {
+    //  this.refs.group.focus();
+    //  return false;
+    //}
+
+    return true;
+  }
+
+  _handleErrorInputChange(e) {
+    this.setState({
+      errorText: e.target.value ? '' : '* 必填',
+    });
+  }
+
+  render() {
+    const { name, desc, group, theme, cover } = this.state;
+    const { onSave, onCancel } = this.props;
+    return (
+      <div>
+        <div className={style.save}>
+          <div className={style.saveLeft}>
+            <TextField ref="name"
+              errorText={this.state.errorText}
+              hintText="请输入场景名称"
+              defaultValue={name}
+              onChange={(e) => this._handleErrorInputChange(e)}
+              floatingLabelText="场景名称" />
+            <TextField ref="desc"
+              multiLine={true}
+              hintText="请输入场景描述"
+              defaultValue={desc}
+              floatingLabelText="场景描述"/>
+            <AutoComplete ref="group"
+              fullWidth={false}
+              floatingLabelText="场景组"
+              hintText="可从已有场景组中选,也可自定义"
+              searchText={group}
+              showAllItems={true}
+              animated={true}
+              dataSource={['12345', '23456', '34567']}
+              onNewRequest={(g) => this.setState({ group: g })} />
+
+          </div>
+          <div className={style.saveRight}>
+            <label style={{color: '#ffffff'}}>场景封面</label>
+            <Slider className={style.slickBox} dots={true} arrows={true} initialSlide={this.getCoverIndex()} adaptiveHeight={true}
+                    lazyLoad={false} infinite={false} speed={500} slidesToShow={1} slidesToScroll={1}
+                    afterChange={ (index) => this.setState({ cover: screenCoverImages[index].name }) } >
+              {
+                screenCoverImages.map(({ img }, index) =>
+                  <div className={style.div} key={index}><img src={img} /></div>
+                )
+              }
+            </Slider>
+          </div>
+        </div>
+        <div style={{
+          position: 'absolute',
+          right: '8px',
+          bottom: '8px',
+        }}>
+          <FlatButton
+            label="取消"
+            secondary={true}
+            onTouchTap={() => onCancel()}
+          />
+          <FlatButton
+            label="提交"
+            primary={true}
+            keyboardFocused={true}
+            onTouchTap={this.handlerSubmit} />
+        </div>
+      </div>
+    );
+  }
+}
+
+SaveWindow.propTypes = {
+  name: PropTypes.string, //场景名称
+  desc: PropTypes.string, //场景描述
+  group: PropTypes.string, //场景组
+  theme: PropTypes.string, //主题
+  cover: PropTypes.string, //场景封面
+  onCancel: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired,
+};
+SaveWindow.defaultProps = {
+  theme: 'TealTheme',
+  group: '未分组',
+  cover: screenCoverImages[0].name,
+};
+
+export default SaveWindow;
+//
+//<div className={style.div}><img src="https://randomuser.me/api/portraits/women/62.jpg" /></div>
+//<div className={style.div}><img src="https://randomuser.me/api/portraits/women/63.jpg" /></div>
+//  <div className={style.div}><img src="https://randomuser.me/api/portraits/women/64.jpg" /></div>
+
+/*
+<SelectField
+  value={theme}
+  onChange={(event, index, value) => this.setState({ theme: value })}
+  floatingLabelText="场景主题">
+  {themes}
+</SelectField>*/
---
+++ src/containers/Maker/index.jsx
@@ -0,0 +1,237 @@
+/**
+ * 定制主界面
+ * @author liekkas
+ */
+import React from 'react';
+import { Dialog, FlatButton } from 'material-ui';
+import _ from 'lodash';
+import { CompsBox, ToolBar, Workspace, Showspace } from '../../components';
+import Home from '../Home/Home';
+import style from './style.scss';
+import { connect } from 'react-redux';
+import { createAction } from '../../actions';
+import { GlobalActionTypes, CurSceneActionTypes, ScenesActionTypes } from '../../constants/ActionTypes';
+
+import { getThemeByName } from '../../tools/styleTools';
+import * as actionCreators from '../../actions';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { saveParabird, updateParabird } from '../../services';
+import SaveWindow from './SaveWindow';
+
+const customStyles = {
+  content: {
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    padding: '0',
+    border: 'none',
+  }
+};
+
+const Maker = React.createClass({
+
+  contextTypes: {
+    muiTheme: React.PropTypes.object,
+    theme: React.PropTypes.string,
+  },
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object,
+    theme: React.PropTypes.string,
+  },
+
+  getInitialState() {
+    const { muiTheme } = getThemeByName(this.props.themeP);
+    return {
+      muiTheme: muiTheme,
+      theme: this.props.themeP,
+      screenRatio: this.props.screenRatio,
+      screenNums: this.props.screenNums,
+      open: false,
+      openSave: false,
+    };
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: this.state.muiTheme,
+      theme: this.state.theme,
+    };
+  },
+
+  componentWillReceiveProps(nextProps, nextState) {
+    //console.log('>>> Maker:WillReceiveRrops:', nextProps, nextState);
+    const { needSave, user, curScene, scenes } = nextProps;
+    const { muiTheme } = getThemeByName(nextProps.themeP);
+    this.setState({ 'theme': nextProps.themeP });
+    this.setState({ 'screenRatio': nextProps.screenRatio });
+    this.setState({ 'screenNums': nextProps.screenNums });
+    this.setState({ 'muiTheme': muiTheme });
+
+    console.log('>>> Maker:WillReceiveRrops', needSave);
+    if (needSave) {
+      user.get('configed') ?
+        updateParabird('root', { curScene, scenes }, this.props.dispatch) :
+        saveParabird('root', { curScene, scenes }, this.props.dispatch);
+    }
+  },
+
+  onThemeChanged(event, index, value) {
+    this.props.dispatch(createAction(GlobalActionTypes.THEME_CHANGED, value));
+  },
+
+  onScreenRatioChanged(event, selectedIndex, menuItem) {
+    this.props.dispatch(createAction(GlobalActionTypes.SCREEN_RATIO_CHANGED, menuItem.payload));
+  },
+
+  onScreenNumChanged(event, selectedIndex, menuItem) {
+    this.props.dispatch(createAction(GlobalActionTypes.SCREEN_NUMS_CHANGED, menuItem.payload));
+  },
+
+  onNewScene() {
+    this.props.dispatch(createAction(CurSceneActionTypes.NEW_SCENE, this.props.user.get('name')));
+  },
+
+  //两处需要保存,当前curScene的,以及scenes里的状态
+  onSaveScene(payload) {
+    this.handleSaveWindowOpen();
+  },
+
+  onPreviewScene() {
+    //this.props.dispatch(createAction(CurSceneActionTypes.PREVIEW_UNSAVED_SCENE));
+    this.setState({ open: true });
+  },
+
+  onDropPlacerOnWorkspace(payload) {
+    this.props.dispatch(createAction(CurSceneActionTypes.DRAG_PLACER_ON_WORKSPACE, payload));
+  },
+
+  onDropComponentIntoWorkspace(payload) {
+    this.props.dispatch(createAction(CurSceneActionTypes.DRAG_COMPONENT_INTO_WORKSPACE, payload));
+  },
+
+  onRemovePlacer(payload) {
+    this.props.dispatch(createAction(CurSceneActionTypes.REMOVE_PLACER_FROM_WORKSPACE, payload));
+  },
+
+  onConfigPlacer(payload) {
+    this.props.dispatch(createAction(CurSceneActionTypes.SAVE_COMPONENT_CONFIG, payload));
+  },
+
+  handleOpen() {
+    this.setState({ open: true });
+  },
+
+  handleClose() {
+    this.setState({ open: false });
+  },
+
+  handleSaveWindowOpen() {
+    this.setState({ openSave: true });
+  },
+  handleSaveWindowClose() {
+    this.setState({ openSave: false });
+  },
+
+  handleSave(config) {
+    //console.log('>>> Marker:handleSave', config);
+    const { dispatch, user, curScene, scenes } = this.props;
+    if (curScene.state === 'unsaved') {
+      curScene.createDate = config.updateDate;
+    }
+    dispatch(createAction(CurSceneActionTypes.SAVE_SCENE, _.merge(curScene, config)));
+    this.handleSaveWindowClose();
+  },
+
+  render() {
+    const appTheme = getThemeByName(this.state.theme).appTheme;
+    const headerBG = { 'background': appTheme.toolbarBgColor };
+    const compsBoxBG = { 'background': appTheme.compsBoxBgColor };
+    const workspaceBG = { 'background': appTheme.workspaceBgColor };
+    const { curScene } = this.props;
+
+    return (
+      <div className={style.myroot}>
+        <div className={style.header} style={headerBG}>
+          <ToolBar onThemeChanged={this.onThemeChanged}
+                   onScreenRatioChanged={this.onScreenRatioChanged}
+                   onScreenNumChanged={this.onScreenNumChanged}
+                   onNewScene={this.onNewScene}
+                   onSaveScene={this.onSaveScene}
+                   onPreviewScene={this.onPreviewScene}/>
+        </div>
+        <div className={style.hbox}>
+          <div className={style.sidebar} style={compsBoxBG}>
+            <CompsBox />
+          </div>
+          <div className={style.workspace} style={workspaceBG}>
+            <Workspace placers={curScene.placers}
+                       screenRatio={this.state.screenRatio}
+                       screenNums={this.state.screenNums}
+                       dropPlacerOnWorkspace={ payload => this.onDropPlacerOnWorkspace(payload)}
+                       dropComponentIntoWorkspace={ payload => this.onDropComponentIntoWorkspace(payload)}
+                       onRemovePlacer={ payload => this.onRemovePlacer(payload) }
+                       onConfigPlacer={ payload => this.onConfigPlacer(payload) }/>
+          </div>
+        </div>
+
+        { this.state.open ? <Showspace isPreview={true}
+                                       scene={curScene}
+                                       bgColor={appTheme.workspaceBgColor}
+                                       onClosePreView={this.handleClose} /> : null
+        }
+
+        {
+          this.state.openSave ? <Dialog
+            title="保存"
+            style={{
+            //width: '75%',
+            //height: '50%',
+            //left: '25%',
+            }}
+            modal={true}
+            open={this.state.openSave}
+            onRequestClose={this.handleSaveWindowClose}>
+            <SaveWindow name={curScene.name}
+                        desc={curScene.desc}
+                        group={curScene.group}
+                        cover={curScene.cover}
+                        onCancel={this.handleSaveWindowClose}
+                        onSave={ (config) => this.handleSave(config) }/>
+          </Dialog> : null
+        }
+      </div>
+    );
+  }
+});
+
+Maker.propTypes = {
+  themeP: React.PropTypes.string.isRequired,
+  screenRatio: React.PropTypes.string.isRequired,
+  screenNums: React.PropTypes.string.isRequired,
+  needSave: React.PropTypes.bool.isRequired,
+  curScene: React.PropTypes.object,
+  scenes: React.PropTypes.object,
+  user: React.PropTypes.object,
+};
+
+Maker.defaultProps = {
+  themeP: 'TealTheme',
+};
+
+function select(state) {
+  //console.log('>>> Maker:Selector', state.get('global'), state.get('curScene'));
+  return {
+    themeP: state.getIn(['global', 'theme']),
+    screenRatio: state.getIn(['global', 'screenRatio']),
+    screenNums: state.getIn(['global', 'screenNums']),
+    user: state.getIn(['global', 'user']),
+    needSave: state.getIn(['global', 'needSave']),
+    curScene: state.get('curScene').toJS(),
+    scenes: state.get('scenes').toJS(),
+  };
+}
+
+export default DragDropContext(HTML5Backend)(connect(select)(Maker));
