afb2e44300b6d3096bcddd6951a3507539e24a7c
liekkas <zjq0717@163.com>
2016-01-08 17:42:12 +0800

    修改单个场景呈现的路由参数,添加一个用户名称的参数

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-
 import { App, Maker, Shower, ShowNav, NotFound } from './containers';
 
 const AppRouter = (
   <Route path="/" component={App}>
     <IndexRoute component={Maker} />
 
-    <Route path="show/:id" component={Shower} />
+    <Route path="show/:userName/:sceneId" component={Shower} />
     <Route path="shownav" component={ShowNav} />
 
     <Route path="*" component={NotFound} status={404}/>
