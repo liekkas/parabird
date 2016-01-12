07836a3f3daba95871033ebd8c5cd8f1045a07c0
liekkas <zjq0717@163.com>
2016-01-12 17:37:15 +0800

    加入base_url

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@
 import { loadingStyles } from './constants/Consts';
 
 export const VERSION = 'v1.0.0';
+export const BASE_URL = 'http://' + window.location.host + '/';
 export const REST_API_BASE_URL = 'http://localhost:4000/api/v1/parabirds/';
 
 //加载效果
