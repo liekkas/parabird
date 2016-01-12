d4bc744baf0e1eb638c6ca9ec13a650c6737f615
liekkas <zjq0717@163.com>
2016-01-12 18:51:01 +0800

    更改Lookup数据结构

--- src/constants/LookUp.js
+++ src/constants/LookUp.js
@@ -1,12 +1,14 @@
 /**
  * Created by liekkas on 15/12/29.
  */
-import * as comp from '../components';
+import * as comps from '../components';
+import defaultImg from '../images/compsnaps/default.png';
+import image from '../images/compsnaps/image.png';
+import singleLine from '../images/compsnaps/singleLine.png';
+
 
 export const Lookup = {
-  LineChart: comp.LineChart,
-  LineChartEditor: comp.LineChartEditor,
-
-  PBImage: comp.PBImage,
-  PBImageEditor: comp.PBImageEditor,
+  LineChart: { name: '线状图', comp: comps.LineChart, editor: comps.LineChartEditor, img: singleLine, },
+  PBImage: { name: '图片', comp: comps.PBImage, editor: comps.PBImageEditor, img: image, },
+  PBClock: { name: '时钟', comp: comps.PBClock, editor: comps.PBClockEditor, img: defaultImg, },
 };
