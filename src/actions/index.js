68d9090ece9548b323f79b943b4115375a486452
liekkas <zjq0717@163.com>
2016-01-12 17:41:22 +0800

    简化action,统一处理

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,16 +2,10 @@
  * Created by liekkas on 15/12/17.
  */
 
-export const THEME_CHANGED = 'THEME_CHANGED';
-
-/**
- * 更换主题
- * @param theme
- * @returns {{type: string, theme: *}}
- */
-export function changeTheme(theme) {
+export function createAction(type, payload = '', meta = '') {
   return {
-    type: THEME_CHANGED,
-    payload: theme
+    type,
+    payload,
+    meta,
   };
 }
