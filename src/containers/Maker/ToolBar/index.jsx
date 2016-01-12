662d2d448bd48ea23e17fb0ee57021771821f084
liekkas <zjq0717@163.com>
2016-01-12 17:35:58 +0800

    工具栏中间呈现当前场景的名称和状态

--- src/containers/Maker/ToolBar/index.jsx
+++ src/containers/Maker/ToolBar/index.jsx
@@ -20,8 +20,8 @@ class ToolBar extends React.Component {
 
   render() {
     //console.log('此刻:', this.context.theme);
-    const { onThemeChanged, onScreenRatioChanged, onScreenNumChanged,
-      onNewScene, onSaveScene, onPreviewScene } = this.props;
+    const { onThemeChanged, curScene, onScreenRatioChanged, onScreenNumChanged,
+      onNewScene, onSaveScene, onScenesMgr, onPreviewScene } = this.props;
 
     return (
       <div className={style.full}>
@@ -31,10 +31,15 @@ class ToolBar extends React.Component {
           <label>Screen Show</label>
         </div>
 
+        <div className={style.title}>
+          <h4>{curScene.name}</h4>
+          <h6>{curScene.state === 'saved' ? '编辑中...' : '新建中...'}</h6>
+        </div>
+
         <div className={style.right}>
           <RaisedButton label="新建" primary={true} onClick={onNewScene}/>
           <RaisedButton label="保存" primary={true} onClick={onSaveScene}/>
-          <RaisedButton label="场景管理" primary={true}/>
+          <RaisedButton label="场景管理" primary={true} onClick={onScenesMgr}/>
           <RaisedButton label="预览" primary={true} onClick={onPreviewScene}/>
           <DropDownMenu value={this.context.theme}
                         onChange={onThemeChanged}>
@@ -51,15 +56,19 @@ ToolBar.contextTypes = {
 };
 
 ToolBar.propTypes = {
+  curScene: PropTypes.object,
+
   onThemeChanged: PropTypes.func.isRequired,
   onScreenRatioChanged: PropTypes.func.isRequired,
   onScreenNumChanged: PropTypes.func.isRequired,
   onSaveScene: PropTypes.func.isRequired,
   onNewScene: PropTypes.func.isRequired,
+  onScenesMgr: PropTypes.func.isRequired,
   onPreviewScene: PropTypes.func.isRequired,
 };
 
 ToolBar.defaultProps = {
+
 };
 
 export default ToolBar;
