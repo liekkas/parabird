a56042f4901f10e1b39c0f230288ee9a18adbaad
liekkas <zjq0717@163.com>
2016-01-12 15:12:11 +0800

    场景管理中实现场景删除和场景组删除,以及场景预览

--- src/containers/Maker/ScenesMgr/index.jsx
+++ src/containers/Maker/ScenesMgr/index.jsx
@@ -1,41 +1,143 @@
 import React, { PropTypes } from 'react';
-import { Popover, Avatar, Card, CardActions, CardHeader, CardMedia, CardTitle, CardText, FlatButton } from 'material-ui';
+import { Popover, Checkbox, FlatButton, RaisedButton } from 'material-ui';
+import { screenCoverImages } from '../../../constants/Consts';
+import { BASE_URL } from '../../../config';
 import style from './style.scss';
+import classNames from 'classnames/bind';
+import Slider from 'react-slick';
+import _ from 'lodash';
 
+const cx = classNames.bind(style);
+
+/**
+ * 场景管理
+ * //TODO Popover有个Bug,当不设置anchorEl时,会报getBoundingClientRect错
+ */
 class ScenesMgr extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      curGroup: props.groups[0].name,
       open: props.open,
+      showAlert: false,
+      deleteType: 'group',
+      needDeleteGroupName: '',
+      needDeleteSceneId: '',
+      alertText: '',
     };
   }
 
+  _handleAlertShow(flag) {
+    this.setState({ showAlert: flag });
+  }
+
+  _handleGroupDelete(groupName) {
+    this.setState({
+      showAlert: true,
+      deleteType: 'group',
+      needDeleteGroupName: groupName,
+      alertText: '真的要删除场景组【' + groupName + '】吗? (注意组内的场景也将一并删除!!!)',
+    });
+  }
+
+  _handleSceneDelete(sceneId, sceneName) {
+    this.setState({
+      showAlert: true,
+      deleteType: 'scene',
+      needDeleteSceneId: sceneId,
+      alertText: '真的要删除场景【' + sceneName + '】吗? ',
+    });
+  }
+
+  _handleConfirm() {
+    this._handleAlertShow(false);
+    const { deleteType, needDeleteGroupName, needDeleteSceneId } = this.state;
+    if (deleteType === 'group') {
+      //如果要删除的group是当前的group,那么向左移,因为第一个是不让删的,肯定有值
+      if (needDeleteGroupName === this.state.curGroup) {
+        const i = _.findIndex(this.props.groups, 'name', needDeleteGroupName);
+        this.setState({ curGroup: this.props.groups[i - 1].name });
+      }
+      this.props.onGroupDelete(needDeleteGroupName);
+    } else {
+      this.props.onSceneDelete(needDeleteSceneId);
+    }
+  }
+
+  _handleGroupClicked(name) {
+    this.setState({ curGroup: name });
+  }
+
+  _getCover(cover) {
+    const obj = _.find(screenCoverImages, 'name', cover);
+    return obj ? obj.img : screenCoverImages[0].img;
+  }
+
   render() {
-    const { onScenesMgrClose } = this.props;
+    const { onScenesMgrClose, groups, user } = this.props;
+    const editClassName = cx('edit', 'zmdi', 'zmdi-edit');
+    const removeClassName = cx('remove', 'zmdi', 'zmdi-delete');
+    const removeGroupClassName = cx('groupRemove', 'zmdi', 'zmdi-delete');
+    const confirmBoxClassName = cx('confirmBox');
+    const { alertText, showAlert, curGroup, curScenes } = this.state;
+
+    console.log('>>> SceneMgr:', curGroup, curScenes, groups, this.props.entries);
     return (
-      <Popover open={true} className={style.root}
+      <Popover zDepth={2} open={true} className={style.root}
                onRequestClose={onScenesMgrClose} >
-        <div style={{ padding:20 }}>
-          <Card className={style.card}>
-            <CardHeader
-              title="元旦节场景"
-              subtitle="最后更新 2016-01-10"
-              avatar={<Avatar style={{color: 'red'}}>元</Avatar>}/>
-            <CardMedia overlay={<CardTitle title="Title" subtitle="Subtitle"/>}>
-              <img src="http://lorempixel.com/600/337/nature/"/>
-            </CardMedia>
-            <CardTitle title="Title" subtitle="Subtitle"/>
-            <CardActions>
-              <FlatButton label="Action1"/>
-              <FlatButton label="Action2"/>
-            </CardActions>
-            <CardText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-            </CardText>
-          </Card>
+        <Slider className={style.slickBox} dots={false} arrows={groups.length > 4} initialSlide={0}
+                lazyLoad={false} infinite={false} speed={500} slidesToShow={4} slidesToScroll={4}>
+          {
+            groups.map(({ name, num }, index) =>
+              <div className={style.wrapper} key={index}>
+                <div className={style.groupCard} style={{
+                  border: curGroup === name ? 'solid 2px white' : 'none',
+                }}>
+                  <FlatButton style={{ width: '90%' }} label={name} primary={true}
+                              onTouchTap={() => this._handleGroupClicked(name)}/>
+                  <label>{num}</label>
+                  { name === '未分组' ? null : <div className={removeGroupClassName} onClick={() => this._handleGroupDelete(name)}/> }
+                </div>
+              </div>
+            )
+          }
+        </Slider>
+
+        <div className={confirmBoxClassName} style={{
+          visibility: showAlert ? 'visible' : 'hidden',
+        }}>
+          <div>{alertText}</div>
+          <FlatButton primary={true} labelStyle={{ color: '#ffff00' }} label="确认"
+                      onClick={() => this._handleConfirm()} />
+          <FlatButton primary={true} labelStyle={{ color: '#ffff00' }} label="取消"
+                      onClick={() => this._handleAlertShow(false)} />
+        </div>
+
+        <Slider className={style.slickBox} dots={true} arrows={_.filter(this.props.entries, { 'group': curGroup }).length > 4} initialSlide={0}
+                lazyLoad={false} infinite={false} speed={500} slidesToShow={4} slidesToScroll={4}>
+          {
+            _.filter(this.props.entries, { 'group': curGroup }).map(({ id, name, updateDate, cover }, index) =>
+              <div className={style.wrapper} key={index}>
+                <div className={style.sceneCard}>
+                  <h4>{name}</h4>
+                  <img src={this._getCover(cover)}
+                       onClick={() => window.open(BASE_URL + '#/show/' + user.name + '/' + id)}/>
+                  <h5>更新于{updateDate.substr(0, 10)}</h5>
+                  <div className={style.toolbox}>
+                    <span className={editClassName}>&nbsp;编辑</span>
+                    <span className={removeClassName}
+                          onClick={() => this._handleSceneDelete(id, name)} >&nbsp;删除</span>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        </Slider>
+
+        <div className={style.sceneDimmer} style={{
+          visibility: showAlert ? 'visible' : 'hidden',
+        }}>
+
         </div>
       </Popover>
     );
@@ -43,8 +145,13 @@ class ScenesMgr extends React.Component {
 }
 
 ScenesMgr.propTypes = {
+  user: PropTypes.object,
   open: PropTypes.bool.isRequired,
+  groups: PropTypes.array.isRequired,
+  entries: PropTypes.array.isRequired,
   onScenesMgrClose: PropTypes.func.isRequired,
+  onSceneDelete: PropTypes.func,
+  onGroupDelete: PropTypes.func,
 };
 ScenesMgr.defaultProps = {
   open: true,
