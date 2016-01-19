34b5d520386dbb771a4d18dd935d86f5665574e4
liekkas <zjq0717@163.com>
2016-01-19 18:19:04 +0800

    add loading style options

--- src/constants/Consts.js
+++ src/constants/Consts.js
@@ -3,17 +3,10 @@
  */
 import React from 'react';
 import { MenuItem } from 'material-ui';
-import festival from '../images/festival.png';
-import event from '../images/event.png';
-import flood from '../images/flood.png';
-import key from '../images/key.png';
-
-//export const themes = [
-//  { payload: 'TealTheme', text: '主题:青葱少年' },
-//  { payload: 'RedTheme', text: '主题:红色风暴' },
-  //{ payload: 'TealTheme', text: '主题:青葱少年' },
-  //{ payload: 'BlueTheme', text: '主题:深空蓝' },
-//];
+import festival from '../images/covers/festival.png';
+import event from '../images/covers/event.png';
+import flood from '../images/covers/flood.png';
+import key from '../images/covers/key.png';
 
 export const themes = [
   <MenuItem key={1} value="TealTheme" primaryText="青葱少年"/>,
@@ -43,3 +36,37 @@ export const screenNums = [
   { payload: '4*6', text: '屏幕数4*6' },
   { payload: '1*1', text: '屏幕数1*1' },
 ];
+
+//加载效果样式
+//详情见http://jonjaques.github.io/react-loaders/
+export const loadingStyles = {
+  ballPulse: 'ball-pulse',
+  ballPulseSync: 'ball-pulse-sync',
+  ballScale: 'ball-scale',
+  ballRotate: 'ball-rotate',
+  ballClipRotate: 'ball-clip-rotate',
+  ballClipRotatePulse: 'ball-clip-rotate-pulse',
+  ballClipRotateMultiple: 'ball-clip-rotate-multiple',
+  ballScaleRipple: 'ball-scale-ripple',
+  ballScaleRippleMultiple: 'ball-scale-ripple-multiple',
+  ballBeat: 'ball-beat',
+  ballScaleMultiple: 'ball-scale-multiple',
+  ballTriangleTrace: 'ball-triangle-trace',
+  ballPulseRise: 'ball-pulse-rise',
+  ballGridBeat: 'ball-grid-beat',
+  ballGridPulse: 'ball-grid-pulse',
+  ballSpinFadeLoader: 'ball-spin-fade-loader',
+  ballSpinLoader: 'ball-spin-loader',
+  ballZigZag: 'ball-zig-zag',
+  ballZigZagDeflect: 'ball-zig-zag-deflect',
+  lineScale: 'line-scale',
+  lineScaleRandom: 'line-scale-random',
+  lineScalePulseOut: 'line-scale-pulse-out',
+  lineScalePulseOutRapid: 'line-scale-pulse-out-rapid',
+  lineSpinFadeLoader: 'line-spin-fade-loader',
+  triangleSkewSpin: 'triangle-skew-spin',
+  SquareSpin: 'square-spin',
+  pacman: 'pacman',
+  cubeTransition: 'cube-transition',
+  semiCircleSpin: 'semi-circle-spin',
+};
