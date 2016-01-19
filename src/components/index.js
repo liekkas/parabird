cd9c3f958180a15d0cf6c377e8fe15ab795cbc6d
liekkas <zjq0717@163.com>
2016-01-19 09:36:48 +0800

    remove LineChart and PBBarChart,use echarts series component instead

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,14 @@
-export { default as LineChart } from './LineChart/LineChart';
-export { default as LineChartEditor } from './LineChart/LineChartEditor';
-
-export { default as PBImage } from './PBImage';
+export { default as PBImage } from './PBImage/PBImage';
 export { default as PBImageEditor } from './PBImage/PBImageEditor';
+
+export { default as PBClock } from './PBClock/PBClock';
+export { default as PBClockEditor } from './PBClock/PBClockEditor';
+
+export { default as PBText } from './PBText/PBText';
+export { default as PBTextEditor } from './PBText/PBTextEditor';
+
+export { default as PBTry } from './PBTry/PBTry';
+export { default as PBTryEditor } from './PBTry/PBTryEditor';
+
+export { default as PBBarChartByECharts } from './echarts/BarChart/BarChart';
+export { default as PBBarChartEditorByECharts } from './echarts/BarChart/BarChartEditor';
