e3991d87a697c19514e9ecf1485c86a43c4b71fa
liekkas <zjq0717@163.com>
2016-01-19 12:12:19 +0800

    help to convert config to echart's option

--- src/components/echarts/helper.js
+++ src/components/echarts/helper.js
@@ -16,6 +16,15 @@ function parseLegend(config) {
   };
 }
 
+function parsePieLegend(config) {
+  return {
+    orient: 'vertical',
+    x: 'left',
+    y: 'bottom',
+    data: _.map(config.data, 'name'),
+  };
+}
+
 function parseXAxis(config) {
   return {
     data: config.labels,
@@ -47,6 +56,33 @@ function parseSeries(config, type) {
   return series;
 }
 
+function parsePieSeries(config, type) {
+  const series = [];
+  series.push({
+    name: config.title,
+    type,
+    data: config.data,
+  });
+  return series;
+}
+
+function parseMapSeries(config, type) {
+  const series = [];
+  _.forEach(config.legend, function (item) {
+    series.push({
+      name: item,
+      type: type,
+      mapType: config.mapType,
+      itemStyle: {
+        normal: {label: {show: true}},
+        emphasis: {label: {show: true}}
+      },
+      data: config[item],
+    });
+  });
+  return series;
+}
+
 /**
  * 生成图形option
  * @param config 一些动态数据
@@ -54,11 +90,34 @@ function parseSeries(config, type) {
  * @param custom 图表独有的一些属性
  */
 export function config2Option(config, type, custom = {}) {
-  return {
-    title: parseTitle(config),
-    legend: parseLegend(config),
-    xAxis: parseXAxis(config),
-    yAxis: parseYAxis(config),
-    series: parseSeries(config, type),
-  };
+  switch (type) {
+    case 'line':
+    case 'bar':
+      return {
+        title: parseTitle(config),
+        legend: parseLegend(config),
+        xAxis: parseXAxis(config),
+        yAxis: parseYAxis(config),
+        series: parseSeries(config, type),
+      };
+    case 'pie':
+      return {
+        title: parseTitle(config),
+        legend: parsePieLegend(config),
+        series: parsePieSeries(config, type),
+      };
+    case 'gauge':
+      return {
+        title: parseTitle(config),
+        series: parsePieSeries(config, type),
+      };
+    case 'map':
+      return {
+        title: parseTitle(config),
+        legend: parseLegend(config),
+        series: parseMapSeries(config, type),
+      };
+    default:
+      return {};
+  }
 }
