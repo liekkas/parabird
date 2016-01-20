a36f7d7dfb8b0cf010e050c213e266796068ac71
liekkas <zjq0717@163.com>
2016-01-20 17:41:25 +0800

    refactor the echart series components , upgrading to 3.0 , and detached initialization data to init.js

--- src/constants/LookUp.js
+++ src/constants/LookUp.js
@@ -7,6 +7,7 @@ import image from '../images/compsnaps/image.png';
 import singleLine from '../images/compsnaps/singleLine.png';
 import singleColumn from '../images/compsnaps/singleColumn.png';
 import singlePie from '../images/compsnaps/single_pie.png';
+import * as init from './init';
 
 /**
  *
@@ -23,139 +24,6 @@ function generate(name, type, comp, editor, initConfig = {}, group = '通用', i
   return { name, type, comp, editor, initConfig, group, img, w, h };
 }
 
-//-------------------- 设置组件初始化数据 --------------------
-//柱图/线图
-function getInitBarChart() {
-  const localData = JSON.stringify({
-    'title': '性能TopN',
-    'subTitle': '从1号到31号',
-    'legend': ['最高值', '最低值'],
-    'labels': ['周一', '周二', '周三', '周四', '周五'],
-    '最高值': [20, 30, 32, 40, 50],
-    '最低值': [10, 20, 22, 30, 40],
-  });
-  return {
-    localData: localData,
-    remoteDataUrl: 'http://localhost:4000/api/v1/chart',
-    mode: 'local',
-  };
-}
-//饼图/环图
-function getInitPieChart() {
-  const localData = JSON.stringify({
-    'title': '无线网',
-    'subTitle': '从1号到31号',
-    'data': [
-      { name: 'A', value: '102' },
-      { name: 'B', value: '90' },
-      { name: 'C', value: '45' },
-      { name: 'D', value: '10' },
-    ],
-  });
-  return {
-    localData: localData,
-    remoteDataUrl: 'http://localhost:4000/api/v1/chart/pie',
-    mode: 'local',
-  };
-}
-//仪表盘
-function getInitGaugeChart() {
-  const localData = JSON.stringify({
-    'title': '均值得分',
-    'subTitle': '从1号到31号',
-    'data': [
-      { name: '大盘数', value: '102' },
-    ],
-  });
-  return {
-    localData: localData,
-    remoteDataUrl: 'http://localhost:4000/api/v1/chart/pie',
-    mode: 'local',
-  };
-}
-//地图
-function getInitMapChart() {
-  const localData = JSON.stringify({
-    'title': '全国电视收视率',
-    'subTitle': '从1号到31号',
-    'mapType': 'china',
-    'legend': ['白天', '晚上'],
-    '白天': [
-      {name: '北京',value: Math.round(Math.random()*10)},
-      {name: '天津',value: Math.round(Math.random()*10)},
-      {name: '上海',value: Math.round(Math.random()*10)},
-      {name: '重庆',value: Math.round(Math.random()*10)},
-      {name: '河北',value: Math.round(Math.random()*10)},
-      {name: '河南',value: Math.round(Math.random()*10)},
-      {name: '云南',value: Math.round(Math.random()*10)},
-      {name: '辽宁',value: Math.round(Math.random()*10)},
-      {name: '黑龙江',value: Math.round(Math.random()*10)},
-      {name: '湖南',value: Math.round(Math.random()*10)},
-      {name: '安徽',value: Math.round(Math.random()*10)},
-      {name: '山东',value: Math.round(Math.random()*10)},
-      {name: '新疆',value: Math.round(Math.random()*10)},
-      {name: '江苏',value: Math.round(Math.random()*10)},
-      {name: '浙江',value: Math.round(Math.random()*10)},
-      {name: '江西',value: Math.round(Math.random()*10)},
-      {name: '湖北',value: Math.round(Math.random()*10)},
-      {name: '广西',value: Math.round(Math.random()*10)},
-      {name: '甘肃',value: Math.round(Math.random()*10)},
-      {name: '山西',value: Math.round(Math.random()*10)},
-      {name: '内蒙古',value: Math.round(Math.random()*10)},
-      {name: '陕西',value: Math.round(Math.random()*10)},
-      {name: '吉林',value: Math.round(Math.random()*10)},
-      {name: '福建',value: Math.round(Math.random()*10)},
-      {name: '贵州',value: Math.round(Math.random()*10)},
-      {name: '广东',value: Math.round(Math.random()*10)},
-      {name: '青海',value: Math.round(Math.random()*10)},
-      {name: '西藏',value: Math.round(Math.random()*10)},
-      {name: '四川',value: Math.round(Math.random()*10)},
-      {name: '宁夏',value: Math.round(Math.random()*10)},
-      {name: '海南',value: Math.round(Math.random()*10)},
-      {name: '台湾',value: Math.round(Math.random()*10)},
-    ],
-    '晚上': [
-      {name: '北京',value: Math.round(Math.random()*10)},
-      {name: '天津',value: Math.round(Math.random()*10)},
-      {name: '上海',value: Math.round(Math.random()*10)},
-      {name: '重庆',value: Math.round(Math.random()*10)},
-      {name: '河北',value: Math.round(Math.random()*10)},
-      {name: '河南',value: Math.round(Math.random()*10)},
-      {name: '云南',value: Math.round(Math.random()*10)},
-      {name: '辽宁',value: Math.round(Math.random()*10)},
-      {name: '黑龙江',value: Math.round(Math.random()*10)},
-      {name: '湖南',value: Math.round(Math.random()*10)},
-      {name: '安徽',value: Math.round(Math.random()*10)},
-      {name: '山东',value: Math.round(Math.random()*10)},
-      {name: '新疆',value: Math.round(Math.random()*10)},
-      {name: '江苏',value: Math.round(Math.random()*10)},
-      {name: '浙江',value: Math.round(Math.random()*10)},
-      {name: '江西',value: Math.round(Math.random()*10)},
-      {name: '湖北',value: Math.round(Math.random()*10)},
-      {name: '广西',value: Math.round(Math.random()*10)},
-      {name: '甘肃',value: Math.round(Math.random()*10)},
-      {name: '山西',value: Math.round(Math.random()*10)},
-      {name: '内蒙古',value: Math.round(Math.random()*10)},
-      {name: '陕西',value: Math.round(Math.random()*10)},
-      {name: '吉林',value: Math.round(Math.random()*10)},
-      {name: '福建',value: Math.round(Math.random()*10)},
-      {name: '贵州',value: Math.round(Math.random()*10)},
-      {name: '广东',value: Math.round(Math.random()*10)},
-      {name: '青海',value: Math.round(Math.random()*10)},
-      {name: '西藏',value: Math.round(Math.random()*10)},
-      {name: '四川',value: Math.round(Math.random()*10)},
-      {name: '宁夏',value: Math.round(Math.random()*10)},
-      {name: '海南',value: Math.round(Math.random()*10)},
-      {name: '台湾',value: Math.round(Math.random()*10)},
-    ]
-  });
-  return {
-    localData: localData,
-    remoteDataUrl: 'http://localhost:4000/api/v1/chart/pie',
-    mode: 'local',
-  };
-}
-
 /**
  * 组件字典
  */
@@ -165,16 +33,22 @@ export const Lookup = {
   PBClock: generate('时钟', 'PBClock', comps.PBClock, comps.PBClockEditor),
   PBText: generate('文本', 'PBText', comps.PBText, comps.PBTextEditor, { title: '标题', text: '正文' }),
   PBTry: generate('试验品', 'PBTry', comps.PBTry, comps.PBTryEditor),
-  //ECharts系列
-  PBBarChartByECharts: generate('柱图', 'PBBarChartByECharts', comps.PBBarChartByECharts, comps.PBBarChartEditorByECharts,
-    getInitBarChart(), '基础图表', singleColumn),
-  PBLineChartByECharts: generate('线图', 'PBLineChartByECharts', comps.PBLineChartByECharts, comps.PBLineChartEditorByECharts,
-    getInitBarChart(), '基础图表', singleLine),
-  PBPieChartByECharts: generate('饼图', 'PBPieChartByECharts', comps.PBPieChartByECharts, comps.PBPieChartEditorByECharts,
-    getInitPieChart(), '基础图表', singlePie),
-  PBGaugeChartByECharts: generate('仪表盘', 'PBGaugeChartByECharts', comps.PBGaugeChartByECharts, comps.PBGaugeChartEditorByECharts,
-    getInitGaugeChart(), '基础图表'),
-  PBMapChartByECharts: generate('地图', 'PBMapChartByECharts', comps.PBMapChartByECharts, comps.PBMapChartEditorByECharts,
-    getInitMapChart(), '基础图表'),
+  //ECharts系列,共用一个组件,但为了方便演示,弄出一系列组件,通过初始值来区分
+  PBBarChartByECharts: generate('柱图', 'PBBarChartByECharts', comps.PBECharts, comps.PBEChartsEditor,
+    init.getInitLineBarChart('bar'), '基础图表', singleColumn),
+  PBLineChartByECharts: generate('线图', 'PBLineChartByECharts', comps.PBECharts, comps.PBEChartsEditor,
+    init.getInitLineBarChart('line'), '基础图表', singleLine),
+  PBPieChartByECharts: generate('饼图', 'PBPieChartByECharts', comps.PBECharts, comps.PBEChartsEditor,
+    init.getInitPieChart(), '基础图表', singlePie),
+  PBScatterChartByECharts: generate('散点图', 'PBScatterChartByECharts', comps.PBECharts, comps.PBEChartsEditor,
+    init.getInitScatterChart(), '基础图表', singlePie),
+  PBGaugeChartByECharts: generate('仪表盘', 'PBGaugeChartByECharts', comps.PBECharts, comps.PBEChartsEditor,
+    init.getInitGaugeChart(), '基础图表'),
+  PBMapChartByECharts: generate('地图', 'PBMapChartByECharts', comps.PBECharts, comps.PBEChartsEditor,
+    init.getInitMapChart(), '基础图表', defaultImg, 45, 50),
+  PBParallelChartByECharts: generate('平行坐标', 'PBParallelChartByECharts', comps.PBECharts, comps.PBEChartsEditor,
+    init.getInitParallelChart(), '基础图表', defaultImg, 40, 40),
+  PBHeatChartByECharts: generate('热力矩阵', 'PBHeatChartByECharts', comps.PBECharts, comps.PBEChartsEditor,
+    init.getInitHeatChart(), '基础图表', defaultImg, 54, 40),
   //D3系列
 };
