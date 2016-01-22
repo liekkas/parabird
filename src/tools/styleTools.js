77e121cba3d83f98fae755f4874605f19929eaea
liekkas <zjq0717@163.com>
2016-01-22 16:52:42 +0800

    update the styletool

--- src/tools/styleTools.js
+++ src/tools/styleTools.js
@@ -1,27 +1,21 @@
 /**
  * Created by liekkas on 15/12/17.
  */
-import { RedTheme, BlueTheme, YellowTheme, TealTheme } from '../styles';
+import { ChinaRed, UniverseBlue, YellowTheme, TealTheme } from '../styles';
 import { ThemeManager } from 'material-ui/lib/styles';
 
 export function getThemeByName(themeName) {
   let theme;
   switch (themeName) {
-    case 'RedTheme':
-      theme = RedTheme;
+    case 'UniverseBlue':
+      theme = UniverseBlue;
       break;
-    case 'BlueTheme':
-      theme = BlueTheme;
-      break;
-    case 'YellowTheme':
-      theme = YellowTheme;
+    case 'ChinaRed':
+      theme = ChinaRed;
       break;
     default:
-      theme = TealTheme;
+      theme = UniverseBlue;
   }
 
-  return {
-    'muiTheme': ThemeManager.getMuiTheme(theme),
-    'appTheme': theme.appTheme,
-  };
+  return theme;
 }
