ad57c99b6e81c33acf8ab8c816b191da1759e9fe
liekkas <zjq0717@163.com>
2016-01-22 16:51:24 +0800

    remove deprecated themes and add two new themes

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,11 +1,6 @@
 /**
  * Created by liekkas on 15/12/12.
  */
-export { default as RedTheme } from './red-theme';
-export { default as BlueTheme } from './blue-theme';
-export { default as YellowTheme } from './yellow-theme';
-export { default as TealTheme } from './teal-theme';
 
-
-
-
+export { default as ChinaRed } from './themes/ChinaRed';
+export { default as UniverseBlue } from './themes/UniverseBlue';
